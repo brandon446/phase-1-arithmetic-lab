667b0a9e7ebcd2bcc12bc7df60b9fc059c293c3e
brandon <brandon.ngongolo@student.moringaschool.com>
2022-11-28 15:42:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1=31
+let num2=2
+const multiply=num1*num2;
+multiply;
+
+let random;
+random=Math.floor(Math.random()*10)+1;
+random;
+
+let num3=22
+let num4=18
+const mod=num3%num4;
+mod;
+
+let max;
+max=Math.max(5,10,15,20);
+max;
